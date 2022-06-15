0bf4958d8ed36ed5fd2991da7f2da3ef5c43949b
parvathi15 <menonparvathi23@gmail.com>
2022-06-15 13:49:08 +0200

    sdfdsf

--- src/Components/CreateList.js
+++ src/Components/CreateList.js
@@ -1,9 +1,173 @@
 import React, { Component } from 'react';
+import axios from "axios";
 
-export default class CreateList extends Component {
+const apiUrl = "https://zerowaste-api.herokuapp.com/api/";
+
+class CreateList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      alternative: "",
+      title: "",
+      description: "",
+      image: "",
+      list: [],
+      message: "",
+      alert: { show: false }
+    };
+    this.timerId = null;
+    this.fileInput = React.createRef();
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onChangeTitle = this.onChangeTitle.bind(this);
+    this.onChangeDescription = this.onChangeDescription.bind(this);
+    this.onChangeImage = this.onChangeImage.bind(this);
+    this.onChangeAlt = this.onChangeAlt.bind(this);
+  }
+  onChangeAlt = e => {
+    this.setState({
+      alternative: e.target.value
+    });
+  };
+
+  onChangeTitle = e => {
+    this.setState({
+      title: e.target.value
+    });
+  };
+
+  onChangeDescription = e => {
+    this.setState({
+      description: e.target.value
+    });
+  };
+
+  onChangeImage = e => {
+    this.setState({
+      image: e.target.files[0]
+    });
+  };
+
+  // onChangeAlert = ({ type, text }) => {
+  //   this.setState({
+  //     show: true,
+  //     type,
+  //     text
+  //   });
+  //   setTimeout(() => {
+  //     this.setState({ show: false });
+  //   }, 3000);
+  // };
+
+  singleFileUpload = async (data, options) => {
+    try {
+      await axios.post(apiUrl + "singleFile", data, options);
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  resetMessage() {
+    this.timerId = setTimeout(() => {
+      this.setState({
+        message: ""
+      });
+      this.timerId = null;
+    }, 2000);
+  }
+  onSubmit = async e => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("file", this.state.image);
+    formData.append("title", this.state.title);
+    formData.append("description", this.state.description);
+    formData.append("alternative", this.state.alternative);
+    this.singleFileUpload(formData);
+    console.log(formData);
+    if (
+      this.state.title !== "" &&
+      this.state.description !== "" &&
+      this.state.alternative !== ""
+    ) {
+      this.setState({
+        message: "Data submitted Successfully."
+      });
+      this.resetMessage();
+    } else {
+      this.setState({
+        message: "Invalid Data"
+      });
+      this.resetMessage();
+    }
+    this.setState({
+      title: "",
+      description: "",
+      alternative: "",
+      image: (this.fileInput.current.value = "")
+    });
+  };
   render() {
+    console.log(this.state.message);
     return (
-      <div><h1>CreateList</h1></div>
-    )
+      <div style={{ marginTop: 20 }} className="container">
+        <h3 className="mb-5">Create New Todo</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control mb-3 mt-2"
+              value={this.state.title}
+              onChange={this.onChangeTitle}
+              name="searchValue"
+              placeholder="title"
+            />
+
+            <textarea
+              className="form-control mb-3 mt-4"
+              value={this.state.description}
+              onChange={this.onChangeDescription}
+              placeholder="description"
+            />
+
+            <input
+              type="text"
+              className="form-control mb-3 mt-4"
+              value={this.state.alternative}
+              onChange={this.onChangeAlt}
+              placeholder="Alternative"
+            />
+            <input
+              type="file"
+              className="form-control mb-3 mt-4"
+              onChange={this.onChangeImage}
+              ref={this.fileInput}
+              placeholder="Image"
+            />
+          </div>
+
+          <div className="form-group">
+            <input
+              type="submit"
+              value="Create Todo"
+              className="btn btn-primary mt-2"
+            />
+          </div>
+        </form>
+        <p>{this.state.message}</p>
+      </div>
+    );
   }
 }
+
+export default CreateList;
+
+// const newTodo = {
+//   title: this.state.title,
+//   description: this.state.description,
+//   alternative: this.state.alternative,
+//   formData
+// };
+// console.log(newTodo);
+// axios
+//   .post("http://localhost:8000/projects/add", newTodo)
+//   .then(res => console.log(res.data));
--- src/Components/ListDetails.js
+++ src/Components/ListDetails.js
@@ -1,9 +1,111 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
-export default class ListDetails extends Component {
+class ListDetails extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchterm: "",
+      list: [],
+      loading: true,
+      display: "none"
+    };
+    // var display = false;
+
+    this.onChangeSearchterm = this.onChangeSearchterm.bind(this);
+    this.getdata = this.getdata.bind(this);
+  }
+
+  onChangeSearchterm(e) {
+    e.preventDefault();
+    this.setState({
+      searchterm: e.target.value
+    });
+  }
+
+  componentDidMount() {
+    // var display = false;
+  }
+
+  getdata = async e => {
+    e.preventDefault();
+    // const url = `http://localhost:8000/desc/${this.state.searchterm}`;
+    // console.log(url);
+    // const api_call = await fetch(url);
+    // console.log(api_call);
+    // const data = await api_call.json();
+    // this.setState({
+    //   list: data
+    // });
+    axios
+      .get(`http://localhost:5000/desc/${this.state.searchterm}`)
+      .then(response => {
+        // this.state.list.push(response.data);
+        this.setState({ list: response.data });
+        this.setState({ display: "block" });
+        console.log(this.state.list);
+      })
+      .catch(error => {
+        this.setState({ display: "none" });
+        console.log(error);
+      });
+  };
+
   render() {
+    console.log(this.state.list);
     return (
-      <div><h1>ListDetails</h1></div>
-    )
+      <div className="container mt-3 ml-5">
+        <form
+          style={{ display: "flex", margin: "40px auto", marginLeft: "83px" }}
+        >
+          <input
+            type="text"
+            className="form-control"
+            style={{ width: "70%" }}
+            value={this.state.searchterm}
+            onChange={this.onChangeSearchterm}
+          />
+          <button className="btn btn-primary" onClick={this.getdata}>
+            search
+          </button>
+        </form>
+        <div className="container mt-3 ml-5">
+          <div class="row" style={{ display: this.state.display }}>
+            {this.state.list.map((cocktail, key) => {
+              return (
+                <div class="col-md-8" style={{ marginBottom: "2rem" }}>
+                  <div className="img-container">
+                    <h4 className="mt-2 mb-5">{cocktail.title}</h4>
+                    <img
+                      src={`http://localhost:8000/${cocktail.filePath}`}
+                      height="200"
+                      className="card-img-top img-responsive"
+                      alt="img"
+                    />
+                    <p
+                      className="mt-2"
+                      style={{ letterSpacing: "3px", color: "#615f5f" }}
+                    >
+                      {cocktail.alternative}
+                    </p>
+                    <Link
+                      to={{
+                        pathname: `/detailpage/${cocktail._id}`,
+                        state: { id: cocktail._id }
+                      }}
+                    >
+                      DETAILS
+                    </Link>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    );
   }
 }
+export default ListDetails;
+
